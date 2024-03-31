0a3fab47de3c949611c1465ec8d443ecaa74c3c6
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-03-31 09:09:31 -0400

    revise entire auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,54 +1,44 @@
-// Import necessary modules
-import { Router } from 'express'
+/import { Router } from 'express'
 import db from '../db.js'
-import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
-
+import jwt from 'jsonwebtoken'
 const router = Router()
 
-const jwtSecret = process.env.JWT_SECRET
+const jwtSecret = process.env.JWTSECRET
 
-// Signup POST or create a new user
 router.post('/signup', async (req, res) => {
-  // Extract user data from request body
-
   try {
-    // Validation checks
-    if (!req.body.email) throw new Error('Email is required')
-    if (!req.body.password) throw new Error('Password is required')
-    if (!req.body.first_name) throw new Error('First name is required')
-    if (!req.body.last_name) throw new Error('Last name is required')
-    if (!req.body.profile_pic) throw new Error('Picture is required')
-    if (req.body.password.length < 6)
-      throw new Error('Password must be at least 6 characters')
+    // Required fields
+    if (!req.body.email) {
+      throw new Error('email is required')
+    }
+    if (!req.body.password) {
+      throw new Error('password is required')
+    }
+    if (!req.body.profile_pic) {
+      throw new Error('profile_pic is required')
+    }
+    if (!req.body.first_name) {
+      throw new Error('first_name is required')
+    }
+    if (!req.body.last_name) {
+      throw new Error('last_name is required')
+    }
+    // Other validation
+    if (req.body.password.length < 6) {
+      throw new Error('password must be minimum 6 characters long')
+    }
+    // Check duplicate user by email
+    const userExists = await db.query(`
+      SELECT * FROM users WHERE email = '${req.body.email}'
+    `)
+    if (userExists.rows.length) {
+      throw new Error('User with this email already exists!')
+    }
 
-    // Check for duplicate email
-    const userExists = await db.query(
-      `SELECT * FROM users WHERE email = '$1'`, // Use parameterized query to prevent SQL injection
-      [req.body.email] // Pass email as a parameter
-    )
-
-    if (userExists.rows.length) throw new Error('User already exists')
-
-    // Hash the password
+    // Hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
-
-    //     // Create the user
-    //     const queryString = `INSERT INTO users (first_name, last_name, email, password, profile_pic)
-    // VALUES ($1, $2, $3, $4, $5)
-    // RETURNING user_id, email`
-
-    //     const values = [
-    //       req.body.first_name,
-    //       req.body.last_name,
-    //       req.body.email,
-    //       hashedPassword,
-    //       req.body.profile_pic
-    //     ]
-
-    // let user = (await db.query(queryString, values)).rows[0]
-
     // Save user
     const { rows } = await db.query(`
       INSERT INTO users (first_name, last_name, email, password, profile_pic)
@@ -56,8 +46,7 @@ router.post('/signup', async (req, res) => {
       RETURNING *
     `)
     let user = rows[0]
-
-    // Creating the token
+    // Create token
     const token = jwt.sign({ user_id: user.user_id }, jwtSecret)
     res.cookie('jwt', token)
 
@@ -71,39 +60,115 @@ router.post('/signup', async (req, res) => {
   }
 })
 
-//LOGIN POST user already in DB
 router.post('/login', async (req, res) => {
-  const { password, email, user_id, first_name, last_name } = req.body
-  let dbpassword = `SELECT * FROM users WHERE users.email = '${email}'`
   try {
-    let { rows } = await db.query(dbpassword)
-
-    const isPswValid = await bcrypt.compare(password, rows[0].password)
-
-    if (rows.length === 0) {
-      throw new Error('User not found or password incorrect')
+    // Required fields
+    if (!req.body.email) {
+      throw new Error('email is required')
     }
-
-    if (isPswValid) {
-      let payload = {
-        email: rows[0].email,
-        user_id: rows[0].user_id
-      }
-
-      let token = jwt.sign(payload, jwtSecret)
-      res.cookie('jwt', token)
-
-      res.json(`${rows[0].last_name} you are logged in`)
+    if (!req.body.password) {
+      throw new Error('password is required')
     }
+    // Find user
+    const { rows } = await db.query(`
+      SELECT * FROM users WHERE email = '${req.body.email}'
+    `)
+    if (!rows.length) {
+      throw new Error('Either your email or your password is incorrect')
+    }
+    const user = rows[0]
+    // Validate password
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    )
+    if (!isPasswordValid) {
+      throw new Error('Either your email or your password is incorrect')
+    }
+    const token = jwt.sign(
+      { user_id: user.user_id, email: user.email },
+      jwtSecret
+    )
+    // Compose response
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none'
+    })
+    // Respond
+    res.json({ message: 'You are logged in' })
   } catch (err) {
     res.json({ error: err.message })
   }
 })
 
-// Logout user
 router.get('/logout', (req, res) => {
-  res.clearCookie('jwt')
-  res.send('You are logged out')
+  try {
+    res.clearCookie('jwt', {
+      secure: true,
+      sameSite: 'none'
+    })
+    res.json({ message: 'You are logged out' })
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+})
+
+router.get('/profile', async (req, res) => {
+  try {
+    // Validate Token
+    const decodedToken = jwt.verify(req.cookies.jwt, jwtSecret)
+    if (!decodedToken || !decodedToken.user_id || !decodedToken.email) {
+      throw new Error('Invalid authentication token')
+    }
+    const { rows: userRows } = await db.query(`
+      SELECT user_id, first_name, last_name, profile_pic, email
+      FROM users WHERE user_id = ${decodedToken.user_id}
+    `)
+    res.json(userRows[0])
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+})
+
+router.patch('/profile', async (req, res) => {
+  try {
+    // Validate Token
+    const decodedToken = jwt.verify(req.cookies.jwt, jwtSecret)
+    if (!decodedToken || !decodedToken.user_id || !decodedToken.email) {
+      throw new Error('Invalid authentication token')
+    }
+    // Validate fields
+    if (
+      !req.body.first_name &&
+      !req.body.last_name &&
+      !req.body.profile_pic &&
+      !req.body.email
+    ) {
+      throw new Error('at least 1 field must be modified')
+    }
+    // Update user
+    let query = `UPDATE users SET `
+    if (req.body.first_name) {
+      query += `first_name = '${req.body.first_name}', `
+    }
+    if (req.body.last_name) {
+      query += `last_name = '${req.body.last_name}', `
+    }
+    if (req.body.email) {
+      query += `email = '${req.body.email}', `
+    }
+    if (req.body.profile_pic) {
+      query += `profile_pic = '${req.body.profile_pic}', `
+    }
+    query = query.slice(0, -2)
+    query += `WHERE user_id = ${decodedToken.user_id} RETURNING profile_pic, first_name, last_name, email, user_id`
+    const { rows: userRows } = await db.query(query)
+    // Respond
+    res.json(userRows[0])
+  } catch (err) {
+    res.json({ error: err.message })
+  }
 })
 
 export default router
