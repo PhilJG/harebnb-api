a1f870ac5dc7d4de6d5b4971998a3f0fde31ac2f
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-04-28 18:54:08 -0400

    fix reviewer profile and adjust lo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,6 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 const router = Router()
 
-
 router.post('/signup', async (req, res) => {
   try {
     // Required fields
@@ -59,39 +58,37 @@ router.post('/signup', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  const { password, email} = req.body.data
-  
+  const { password, email } = req.body
+
   let dbpassword = `SELECT * FROM users WHERE users.email = '${email}'`
-  
+
   try {
     let { rows } = await db.query(dbpassword)
-    
+
     const isPswValid = await bcrypt.compare(password, rows[0].password)
 
-    if(rows.length === 0){
+    if (rows.length === 0) {
       throw new Error('User not found incorrect')
     }
 
-    if(isPswValid){
+    if (isPswValid) {
       let payload = {
         email: rows[0].email,
         user_id: rows[0].user_id
-      }      
-      
+      }
+
       let token = jwt.sign(payload, process.env.JWT_SECRET)
-      
-      
+
       res.cookie('jwt', token)
-      
+
       res.json(`${rows[0].last_name} you are logged in`)
     }
-    } catch (err){
-      console.log(err);
-      
-      res.json({error: err})
-    }
+  } catch (err) {
+    console.log(err)
+
+    res.json({ error: err })
   }
-)    
+})
 
 router.get('/logout', (req, res) => {
   try {
@@ -99,7 +96,7 @@ router.get('/logout', (req, res) => {
       secure: true,
       sameSite: 'none'
     })
- 
+
     res.json({ message: 'You are logged out' })
   } catch (err) {
     res.json({ error: err.message })
@@ -107,9 +104,7 @@ router.get('/logout', (req, res) => {
 })
 
 router.get('/profile', async (req, res) => {
-  
   try {
-    
     // Validate Token
     const decodedToken = jwt.verify(req.cookies.jwt, process.env.JWT_SECRET)
 
@@ -120,7 +115,7 @@ router.get('/profile', async (req, res) => {
       SELECT user_id, first_name, last_name, profile_pic, email
       FROM users WHERE user_id = ${decodedToken.user_id}
     `)
-    
+
     res.json(userRows[0])
   } catch (err) {
     res.json({ error: err.message })
